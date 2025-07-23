8ea3c6234ca329d95cf902f6ba3396891092a2bf
Risfa <risfandhiani@gmail.com>
2025-07-23 13:36:38 +0700

    docker pratices

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Express Backend!');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
